9c13716a0e6dc259e75b0167433445bed82b7042
Shukri Isse <shukri.isse1234@gmail.com>
2025-07-13 19:36:12 -0500

    add logic for calculating age

--- script.js
+++ script.js
@@ -25,6 +25,51 @@ const showErrMsg = (err, message, inputElement, inputElmntLabel) => {
     inputElmntLabel.classList.add("error");
 };
 
+//Calculate Age
+const calculateAge = (day, month, year) => {
+    const today = new Date() // current date
+    const currYear = today.getFullYear();
+    const currMonth = today.getMonth(); //make user input match date() which starts jan at 0 index
+    const currDay = today.getDate(); //day of the month
+
+    // convert month (1-12) into zero based index (0-11)
+    const birthMonth = parseInt(month) -1;
+
+    //Calculate Day Differnce
+    let dayDifference = currDay - parseInt(day);
+    //Calculate Month Difference
+    let monthDifference = currMonth - birthMonth;
+    // Calculate Year Difference
+    let yearDifference =  currYear - parseInt(year);
+
+    // if day difference is negative then birth DAY has not happened yet this month
+    // you are not a month older yet, so subtract 1 from month
+    if (dayDifference < 0) {
+        monthDifference --;
+        // create a date with currMonth as the *next* month and 0 as day → gives last day of *previous* month
+        //  and 0 for day to access last day of previous month
+        let lastDayOfPreviousMonth = new Date(currYear, currMonth, 0);
+        // get the day of the month aka number of days in that month
+        let numberOfDaysInPreviousMonth = lastDayOfPreviousMonth.getDate();
+        //reset the day by adding number of days in previous month
+        // ex if -3 days and 30 days in last month then -> -3 + 30 or 27 days
+        dayDifference += numberOfDaysInPreviousMonth;
+    }
+
+    // if month difference is negative then birth MONTH has not happened yet this year
+    // you are not a year older yet, so subtract 1 from year
+    // reset the month by adding 12
+    if (monthDifference < 0) {
+        yearDifference --;
+        monthDifference += 12;
+    }
+
+    // console.log(`yearDifference ${yearDifference}`) 
+    // console.log(`monthDifference ${monthDifference}`)
+    // console.log(`dayDifference ${dayDifference}`)
+    return {yearDifference, monthDifference, dayDifference};
+}
+
 myForm.addEventListener("submit", (event) => {
     event.preventDefault();
 
@@ -79,18 +124,9 @@ myForm.addEventListener("submit", (event) => {
         }
     }
 
-//  calculateAge(birthDayInput, birthMonthInput,birthYearInput);
-    
+// destructure to save/return 3 values for year month and day
+const {yearDifference, monthDifference, dayDifference} = calculateAge(birthDayInput, birthMonthInput, birthYearInput);
+console.log(yearDifference);
+console.log(monthDifference);
+console.log(dayDifference);
 });
-
-
-const calculateAge = (day, month, year) => {
-    const date = new Date() // current date
-    const currYear = date.getFullYear();
-    const currMonth = date.getMonth();
-    const currDay = date.getDay();
-
-    const yearDifference =  currYear - parseInt(year);
-    //console.log(yearDifference) //25
-
-}
